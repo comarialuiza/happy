cb0986adbe79ee3542c6c1da6262d0afc7915950
Maria Luiza <co.maria.luiza@gmail.com>
2020-10-14 22:38:56 -0300

    Creates controllers, models and connection to database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
+import './database/connection';
+
+import routes from './routes';
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-    return res.json({ message: 'Hello World' });
-})
+app.use(routes);
 
 app.listen(3333);

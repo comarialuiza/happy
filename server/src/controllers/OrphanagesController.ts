568c563a57486d2d93ac56c6007ddddcbd70fc15
Maria Luiza <co.maria.luiza@gmail.com>
2020-10-15 10:41:16 -0300

    Add images

--- server/src/controllers/OrphanagesController.ts
+++ server/src/controllers/OrphanagesController.ts
@@ -32,8 +32,13 @@ export default {
             opening_hours, 
             open_on_weekends 
         } = req.body;
-    
+        
         const orphanagesRepository = getRepository(Orphanage);
+
+        const requestImages = req.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
     
         const orphanage = orphanagesRepository.create({
             name, 
@@ -42,7 +47,8 @@ export default {
             about, 
             instructions, 
             opening_hours, 
-            open_on_weekends 
+            open_on_weekends,
+            images
         });
     
         await orphanagesRepository.save(orphanage);
